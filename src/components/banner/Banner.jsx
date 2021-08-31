6be4af907fc8ef662a9ab1d40df768fe20c34c11
Christian Marquez <christianmarquez41@gmail.com>
2021-08-30 21:55:40 -0400

    added fade effect from banner to list of movies

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,4 +1,4 @@
-import "./banner.css"
+import './banner.css';
 import { useEffect, useState } from 'react';
 import axios from '../../axios';
 import requests from '../../request';
@@ -19,7 +19,7 @@ export default function Banner() {
   }, []);
 
   function truncate(str, n) {
-      return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
   }
 
   return (
@@ -32,13 +32,16 @@ export default function Banner() {
       }}
     >
       <div className="banner-contents">
-        <h1 className="banner-title">{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner-buttons">
           <button className="banner-button">Play</button>
           <button className="banner-button">My List</button>
         </div>
         <h1 className="banner-description">{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className="banner-fadeBottom"></div>
     </header>
   );
 }
